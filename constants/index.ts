3a0e3e9af6ed263a34ca3b7eadaff38c8dff5d30
Gerald <geraldpng@gmail.com>
2021-08-16 00:00:40 +0800

    Add GA

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,2 @@
 export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+import { GA_ID } from 'constants/index';
+
+class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          {GA_ID && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', ${GA_ID});
+              `,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
