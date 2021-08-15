9604f07d21d07ad33666b8b0d5829ea73c32fc6c
Gerald <geraldpng@gmail.com>
2021-08-16 00:18:09 +0800

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,29 @@ const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
       <NextSeo
         title="Meet Go Where?"
         description="Can't decide where is most convenient place to meet? Enter your postal codes and find out"
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/favicon-32x32.png',
+          },
+          {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/favicon-16x16.png',
+          },
+          {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/apple-touch-icon.png',
+          },
+          {
+            rel: 'manifest',
+            href: '/site.webmanifest',
+          },
+        ]}
       />
       <ChakraProvider resetCSS>
         <Component {...pageProps} />
